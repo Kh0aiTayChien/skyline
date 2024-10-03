24ae9d708e627f81d5f0b28a23b12a7dc6b9826f
chienlv <chienlv@amcolab.vn>
2024-10-03 14:15:17 +0700

    submit email setting fix final

--- app/Http/Controllers/HomePage/IndexController.php
+++ app/Http/Controllers/HomePage/IndexController.php
@@ -68,8 +68,8 @@ class IndexController extends Controller
         $phone = $request->phone;
         $email = $request->email;
         $detail = $request->detail;
-//        Mail::to('thanhnam8921@gmail.com')->send(new RegisterMailable($name, $phone, $email, $detail));
-        Mail::to('chien.hcckt@gmail.com')->send(new RegisterMailable($name, $phone, $email, $detail));
+        Mail::to('thanhnam8921@gmail.com')->send(new RegisterMailable($name, $phone, $email, $detail));
+//        Mail::to('chien.hcckt@gmail.com')->send(new RegisterMailable($name, $phone, $email, $detail));
         return response()->json($viewData);
     }
 }
